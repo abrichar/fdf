94be5eeaa0f7cce8b0e156f4d1878d6ad36fbbe0
Antoine BRICHARD <abrichar@e2r1p11.42.fr>
2017-06-16 18:40:28 +0200

    MaJ

--- main.c
+++ main.c
@@ -6,13 +6,12 @@
 /*   By: abrichar <abrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/16 16:23:25 by abrichar          #+#    #+#             */
-/*   Updated: 2017/06/16 18:17:04 by abrichar         ###   ########.fr       */
+/*   Updated: 2017/06/16 18:38:25 by abrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "libft/includes/libft.h"
 
 int key_react(int keycode, void **tab)
 {
@@ -27,8 +26,14 @@ int key_react(int keycode, void **tab)
 
 int parsing(int fd)
 {
+	int ret;
+	char *line;
 
-	while((ret = read(fd,
+	while (get_next_line(fd, &line) == 1)
+	{
+		printf("%s\n", line);
+	}
+	return (0);
 }
 
 int main(int argc, char **argv)
@@ -40,11 +45,13 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 		return (0);
-	fd = open(name, O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (0);
 	else
 		parsing(fd);
+	if (close(fd) == -1)
+		ft_putstr("Erreur dans la fermeture du fichier");
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 1000, 1000, "fdf");
 	tab[0] = mlx;
