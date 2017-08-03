4467a115478f39ad03ddb02c6347763cde6a314c
Antoine BRICHARD <abrichar@e3r1p1.42.fr>
2017-08-03 15:18:57 +0200

    MAJ

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: abrichar <abrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/16 17:37:30 by abrichar          #+#    #+#             */
-/*   Updated: 2017/07/27 17:38:28 by abrichar         ###   ########.fr       */
+/*   Updated: 2017/08/03 14:02:17 by abrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,38 @@ void	draw(int **tab, int **tab_pars)
 	index[1] = 0;
 	pixel[1] = 200;
 	mlx_pixel_put(tab[0], tab[1], pixel[0], pixel[1], 0x00FF00FF);
-	index[1]++;
 	if (!tab_pars)
 		ft_putstr("prout");
-	while (index[1] != 10)
+	while (tab_pars[index[0]][index[1]])
 	{
-		pixel[0] = 200;
-		to_go = pixel[0] + 100;
-		while (pixel[0] <= to_go)
-		{
-			mlx_pixel_put(tab[0], tab[1], pixel[0], pixel[1], 0x00FF00FF);
-			pixel[0]++;
-		}
+		if (tab_pars[index[0]][index[1] + 1])
+			draw_line(tab_pars[index[0]][index[1]],
+					tab_pars[index[0][index[1] + 1]));
+		if (tab_pars[index[0] + 1][index[1]])
+			draw_line(tab_pars[index[0]][index[1]],
+					tab_pars[index[0] + 1][index[1]]);
 		index[1]++;
-		pixel[1]++;
+		if (!tab_pars[index[0]][index[1]])
+		{
+			index[0]++;
+			index[1] = 0;
+		}
+	}
+}
+
+void	draw_line(int **tab, int *pixel_un, int *pixel_deux)
+{
+	int y1;
+	int x;
+	float y;
+
+	y1 = 0;
+	y = 0;
+	x = 0;
+	while (x < pixel)
+	{
+		y = 0,7 * x + y1;
+		mlx_pixel_put(tab[0], tab[1], )
+		x++;
 	}
 }
