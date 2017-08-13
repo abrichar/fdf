da706f19eee8858b7af36c3cf9844d341b2b7695
Antoine BRICHARD <abrichar@e3r2p10.42.fr>
2017-08-13 20:50:00 +0200

    MAJ

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: abrichar <abrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 16:08:59 by abrichar          #+#    #+#             */
-/*   Updated: 2017/08/12 17:39:55 by abrichar         ###   ########.fr       */
+/*   Updated: 2017/08/13 20:47:23 by abrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,23 @@
 # include "../libft/includes/libft.h"
 # define PIXEL_Y 1000
 # define PIXEL_X 750
+# define COLOR 0x0000FFFF
 
 typedef struct s_pixel
 {
 	int x;
 	int y;
 }				t_pixel;
+
+typedef struct s_line
+{
+	int dx;
+	int sx;
+	int dy;
+	int sy;
+	int err;
+	int e2;
+}				t_line;
 
 typedef struct	s_map
 {
@@ -36,5 +47,6 @@ int		main (int argc, char **argv);
 int		parsing (int fd, t_map *map);
 int		call_for_malloc(int fd, t_map *map, char *file);
 void	display_spot(t_map *map, void **tab);
+void	line(t_pixel pixel0, t_pixel pixel1, void **tab);
 
 #endif
--- src/draw.c
+++ src/draw.c
@@ -6,31 +6,83 @@
 /*   By: abrichar <abrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/12 16:52:49 by abrichar          #+#    #+#             */
-/*   Updated: 2017/08/12 18:11:34 by abrichar         ###   ########.fr       */
+/*   Updated: 2017/08/13 20:48:15 by abrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	display_spot(t_map *map, void **tab)
+static void	draw_vertical(t_pixel pixel, t_map *map, int begin, void **tab)
 {
-	int x;
-	int y;
-	t_pixel p_active;
+	t_pixel	pixel_next;
 
-	p_active.y = 50;
-	y = 0;
-	while (y < map->max_y)
+	pixel_next.x = pixel.x;
+	pixel_next.y = pixel.y + ((PIXEL_X - (begin * 2)) / (map->max_y - 1));
+	line (pixel, pixel_next, tab);
+}
+
+static void	draw_horizontal(t_pixel pixel, t_map *map, int begin, void **tab)
+{
+	t_pixel pixel_next;
+
+	pixel_next.x = pixel.x + ((PIXEL_Y - (begin * 2)) / (map->max_x - 1));
+	pixel_next.y = pixel.y;
+	line (pixel, pixel_next, tab);
+}
+
+void		line(t_pixel pixel0, t_pixel pixel1, void **tab)
+{
+	t_line	line;
+
+	line.dx = abs(pixel1.x - pixel0.x);
+	line.sx = (pixel0.x < pixel1.x) ? 1 : -1;
+	line.dy = abs(pixel1.y - pixel0.y);
+	line.sy = (pixel0.y < pixel1.y) ? 1 : -1;
+	line.err = ((line.dx > line.dy) ? line.dx : -(line.dy))/2;
+	while (1)
 	{
-		x = 0;
-		p_active.x = 50;
-		while (x < map->max_x)
+		mlx_pixel_put(tab[0], tab[1], pixel0.x, pixel0.y, COLOR);
+		if (pixel0.x == pixel1.x && pixel0.y == pixel1.y)
+			break;
+		line.e2 = line.err;
+		if (line.e2 > -(line.dx))
 		{
-			mlx_pixel_put(tab[0], tab[1], p_active.x, p_active.y, 0x0000FFFF);
-			p_active.x += 10;
-			x++;
+			line.err -= line.dy;
+			pixel0.x += line.sx;
 		}
-		y++;
-		p_active.y += 10;
+		if (line.e2 < line.dy)
+		{
+			line.err += line.dx;
+			pixel0.y += line.sy;
+		}
+	}
+}
+
+void		display_spot(t_map *map, void **tab)
+{
+	int		i;
+	int		j;
+	t_pixel p_active;
+	int		begin;
+
+	begin = 5;
+	p_active.y = begin;
+	j = 0;
+	while (j < map->max_y)
+	{
+		i = 0;
+		p_active.x = begin;
+		while (i < map->max_x)
+		{
+			mlx_pixel_put(tab[0], tab[1], p_active.x, p_active.y, COLOR);
+			if (i + 1 < map->max_x)
+				draw_horizontal(p_active, map, begin, tab);
+			if (j + 1 < map->max_y)
+				draw_vertical(p_active, map, begin, tab);
+			p_active.x += ((PIXEL_Y - (begin * 2)) / (map->max_x - 1));
+			i++;
+		}
+		j++;
+		p_active.y += ((PIXEL_X - (begin * 2)) / (map->max_y - 1));
 	}
 }
